13b3208c8d902f78646b5573b17d8235bc1edb3b
ysdex <hancw0218@naver.com>
2025-05-28 21:44:25 +0900

    시작

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("게임 종료");
+    }
+}
