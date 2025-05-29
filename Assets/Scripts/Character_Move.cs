f2955b001e4ec128ac435fe05bb8ace97b394d1b
sangwookim12 <fireman12208@dongguk.ac.kr>
2025-05-29 20:09:19 +0900

    Revert "Merge branch 'master' into sangwoo2"
    This reverts commit dcb9216cdb656109c73d6f7be72c2ee6a0cb0740, reversing
    changes made to 9705dbe50819a663767e6be5719dbe2af08b475b.

--- Assets/Scripts/Character_Move.cs
+++ Assets/Scripts/Character_Move.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Character_Move : MonoBehaviour
 {
@@ -9,21 +10,24 @@ public class Character_Move : MonoBehaviour
     private BoxCollider2D boxCollider;
     public LayerMask layerMask;
 
-    public float speed = 0.1f;              // 기본 이동 속도
-    public float runSpeed = 0.05f;          // 달리기 추가 속도
-    public int walkCount = 20;              // 걸음 수
-
-    private float applyRunSpeed;
-    private bool applyRunFlag = false;
-    private int currentWalkCount;
-    private bool canMove = true;
+    public float speed;
 
     private Vector3 vector;
+
+    public float runSpeed;
+    private float applyRunSpeed;
+    private bool applyRunFlag = false;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool canMove = true;
+
     private Animator animator;
 
     private void Awake()
     {
-        if (Instance == null)
+        if(Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
@@ -39,88 +43,92 @@ public class Character_Move : MonoBehaviour
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
-        animator = GetComponent<Animator>();
-
-        // 디버깅: 값 확인
-        Debug.Log("speed: " + speed + ", walkCount: " + walkCount);
+       animator = GetComponent<Animator>();
     }
 
     IEnumerator MoveCoroutine()
     {
-        while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        while(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
-            applyRunFlag = Input.GetKey(KeyCode.LeftShift);
-            applyRunSpeed = applyRunFlag ? runSpeed : 0;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else
+            {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
 
             vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
 
-            // 대각선 방지
             if (vector.x != 0)
                 vector.y = 0;
 
             animator.SetFloat("DirX", vector.x);
             animator.SetFloat("DirY", vector.y);
 
-            // 충돌 체크
+            RaycastHit2D hit;
+
             Vector2 start = transform.position;
             Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount);
-            Debug.DrawLine(start, end, Color.red, 0.5f); // 화면에 선 표시
 
             boxCollider.enabled = false;
-            RaycastHit2D hit = Physics2D.Linecast(start, end, layerMask);
+            hit = Physics2D.Linecast(start, end , layerMask);
             boxCollider.enabled = true;
 
             if (hit.transform != null)
-            {
-                Debug.Log("이동 중단: 충돌 감지 - " + hit.transform.name);
                 break;
-            }
 
             animator.SetBool("Walking", true);
 
             while (currentWalkCount < walkCount)
             {
-                transform.Translate(vector.x * (speed + applyRunSpeed), vector.y * (speed + applyRunSpeed), 0);
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
                 if (applyRunFlag)
                     currentWalkCount++;
                 currentWalkCount++;
                 yield return new WaitForSeconds(0.01f);
             }
-
-            currentWalkCount = 0;
+            currentWalkCount = 0;          
         }
-
         animator.SetBool("Walking", false);
+
         canMove = true;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (canMove && (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0))
+        if(canMove)
         {
-            canMove = false;
-            StartCoroutine(MoveCoroutine());
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
         }
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (GameManager.Instance != null)
-        {
-            transform.position = GameManager.Instance.nextSpawnPosisition;
-            Debug.Log("씬 전환 후 위치: " + GameManager.Instance.nextSpawnPosisition);
-        }
-        else
-        {
-            Debug.LogWarning("GameManager.Instance가 null입니다.");
-        }
+        transform.position = GameManager.Instance.nextSpawnPosisition;
     }
 }
