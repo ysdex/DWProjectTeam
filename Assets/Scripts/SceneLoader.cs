3c27d02330772fc0aace69f7f605005d8f006793
ysdex <hancw0218@naver.com>
2025-05-30 15:31:35 +0900

    이미지 수정 오디오 중복 수정

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,8 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public void RetryGame()
+    {
+        if (Character_Move.Instance != null)
+        {
+            Destroy(Character_Move.Instance.gameObject);
+            Character_Move.Instance = null;
+        }
+
+        GameObject existingCamera = GameObject.FindWithTag("MainCamera");
+        if (existingCamera != null)
+        {
+            Destroy(existingCamera);
+        }
+
+        SceneManager.LoadScene("Start");
+    }
+
     public void LoadScene(string sceneName)
     {
+        if(Character_Move.Instance != null)
+        {
+            Destroy(Character_Move.Instance.gameObject);
+        }
         SceneManager.LoadScene(sceneName);
     }
 
